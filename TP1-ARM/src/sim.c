7670a86353b781f5805665e1bf3831edda38bece
Naza Gonella <nazagonella2@gmail.com>
2025-03-26 13:45:43 -0300

    EOR

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -153,11 +153,25 @@ void ands_extended(uint32_t instruction) {
     uint32_t Rm = get_Rm(instruction);
     int64_t result = CURRENT_STATE.REGS[Rn] & CURRENT_STATE.REGS[Rm];
     NEXT_STATE.REGS[Rd] = result;
-    if (result == 0) {
-        NEXT_STATE.FLAG_Z = 1;
-    } else if (result < 0){
-        NEXT_STATE.FLAG_N = 1;
-    }
+    // if (result == 0) {
+    //     NEXT_STATE.FLAG_Z = 1;
+    // } else if (result < 0){
+    //     NEXT_STATE.FLAG_N = 1;
+    // }
+    NEXT_STATE.PC += 4;
+}
+
+void eor_extended(uint32_t instruction) {
+    uint32_t Rn = get_Rn(instruction);
+    uint32_t Rd = get_Rd(instruction);
+    uint32_t Rm = get_Rm(instruction);
+    int64_t result = CURRENT_STATE.REGS[Rn] ^ CURRENT_STATE.REGS[Rm];
+    NEXT_STATE.REGS[Rd] = result;
+    // if (result == 0) {
+    //     NEXT_STATE.FLAG_Z = 1;
+    // } else if (result < 0){
+    //     NEXT_STATE.FLAG_N = 1;
+    // }
     NEXT_STATE.PC += 4;
 }
 
@@ -183,7 +197,7 @@ void process_instruction(){
         case (0b10101011000) : printf("INST ADDS (extended register)\n\n");             adds_extended(instruction); break;
         case (0b11101011000) : printf("INST SUBS (extended register)\n\n");             subs_extended(instruction); break;
         case (0b11101010000) : printf("INST ANDS (shifted register, shift '00')\n\n");  ands_extended(instruction); break;
-        case (0b11001010000) : printf("INST EOR (shifted register, shift '00')\n\n"); break;
+        case (0b11001010000) : printf("INST EOR (shifted register, shift '00')\n\n");    eor_extended(instruction); break;
         case (0b10101010000) : printf("INST ORR (shifted register, shift '00')\n\n"); break;
         case (0b11010010100) : printf("INST MOVZ (hw '00')\n\n");                                movz(instruction); break;
         case (0b10001011001) : printf("INST ADD (extended register)\n\n"); break;
