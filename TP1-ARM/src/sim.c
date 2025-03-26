a3f5a66cd6c78257526d4736b91938dc42ce06fc
Naza Gonella <nazagonella2@gmail.com>
2025-03-25 22:06:24 -0300

    SUBS IMMEDIATE NO SHIFT

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -5,62 +5,69 @@
 #include <inttypes.h>
 #include "shell.h"
 
-#define R_OPCODE 0xFFE00000
-#define I_OPCODE 0xFFC00000
-#define D_OPCODE 0xFFE00000
-#define B_OPCODE 0xF8000000
-#define CB_OPCODE 0xFE000000
-#define IW_OPCODE 0xFFE00000
-#define MASCARA 0xFFFFFFFF
+#define R_OPCODE_MASK 0xFFE00000
+#define I_OPCODE_MASK 0xFFC00000
+#define D_OPCODE_MASK 0xFFE00000
+#define B_OPCODE_MASK 0xF8000000
+#define CB_OPCODE_MASK 0xFE000000
+#define IW_OPCODE_MASK 0xFFE00000
+#define MASCARA_MASK 0xFFFFFFFF
 
-#define ADDS_CODE 0xb1000000            //ninguna es prefijo de otra --> esto es lo que nos permite identificar correctamente la inst iterando por todos los tipos
-#define SUBS_CODE
-#define HLT_CODE
-#define CMP_CODE
-#define ANDS_CODE
-#define EOR_CODE
-#define ORR_CODE
-#define CMP_CODE
-#define B_CODE
+#define ADDS_IMM_00_CODE 0xB1000000            //ninguna es prefijo de otra --> esto es lo que nos permite identificar correctamente la inst iterando por todos los tipos
+#define ADDS_IMM_01_CODE 0xB1C00000
+#define ADDS_EXT_CODE 0xAB200000
+#define SUBS_IMM_00_CODE 0x3C400000
+#define SUBS_IMM_01_CODE 0x3C500000
+#define SUBS_EXT_CODE 0x3AC80000  // es igual que cmp ext
+#define HLT_CODE 0x35100000
+#define CMP_EXT_CODE 0x3AC80000 // es igual que sub ext
+#define CMP_IMM_00_CODE 0x3C400000  // es igual que subs imm 00
+#define CMP_IMM_01_CODE 0x3C500000  // es igual que subs imm 01
+#define ANDS_CODE 0x3A800000
+#define EOR_CODE 0x32800000
+#define ORR_CODE 0x2A800000
+#define B_CODE 0x14000000
 #define BR_CODE
-#define BCOND_CODE
-#define LSL_CODE
-#define LSR_CODE
+#define BCOND_CODE 0x54000000
+#define LSL_IMM_CODE 0xD3000000 //?
+#define LSR_IMM_CODE 0x         //?
 #define STUR_CODE
 #define STURB_CODE
 #define STURH_CODE
 #define LDUR_CODE
 #define LDURH_CODE
 #define LDURB_CODE
-#define MOVZ_CODE
-#define ADD_CODE
+#define MOVZ_CODE 0xD2800000
+#define ADD_IMM_00_CODE 0x91000000
+#define ADD_IMM_01_CODE 0x91400000
+#define ADD_EXT_CODE
 #define MUL_CODE
-#define CBZ_CODE
-#define CBNZ_CODE
+#define CBZ_CODE 0xB4000000
+#define CBNZ_CODE 0xB5000000
 
-uint32_t get_R_opcode(uint32_t instruction) {
+uint32_t get_R_opcode(uint32_t instruction){
     return (instruction & R_OPCODE_MASK) >> 21;
 }
 
-uint32_t get_I_opcode(uint32_t instruction) {
+uint32_t get_I_opcode(uint32_t instruction){
     return (instruction & I_OPCODE_MASK) >> 22;
 }
 
 uint32_t get_instruction_bit_field(uint32_t instruction, int size, int shift){
     uint32_t bit_mask = ((1 << (size))-1)<<shift;
-    printf("ESTO: %x\n", bit_mask);
-    return 0;
+    // printf("ESTO: %x\n", bit_mask);
+    return (instruction & bit_mask) >> shift;
 }
 
 uint32_t get_Rn(uint32_t instruction){
-    return get_instruction_bit_field(instruction, 5, 4);
+    return get_instruction_bit_field(instruction, 5, 5);
 }
 
 uint32_t get_Rd(uint32_t instruction){
     return get_instruction_bit_field(instruction, 5, 0);
 }
 
-void adds_immediate(uint32_t instruction, int shifted) {
+void adds_immediate(uint32_t instruction, int shifted){
     if (shifted){
 
     }else {
@@ -70,8 +77,30 @@ void adds_immediate(uint32_t instruction, int shifted) {
     }
 }
 
-void process_instruction()
-{
+void subs_immediate(uint32_t instruction, int shifted){
+    if (shifted){
+
+    }else {
+        uint32_t imm12 = get_instruction_bit_field(instruction, 12, 10);
+        uint32_t Rn = get_Rn(instruction);
+        uint32_t Rd = get_Rd(instruction);
+        printf("\n");
+        printf("Instruction: %x\n", instruction);
+        printf("imm12: %x\n", imm12);
+        printf("Rn   : %x\n", Rn);
+        printf("Rd   : %x\n", Rd);
+        int64_t result = CURRENT_STATE.REGS[Rn] - imm12;
+        printf("\nRESULT: %ld\n\n", result);
+        NEXT_STATE.REGS[Rd] = result;
+        if (result == 0) {
+            NEXT_STATE.FLAG_Z = 1;
+        } else if (result < 0) {
+            NEXT_STATE.FLAG_N = 1;
+        }
+    }
+}
+
+void process_instruction(){
     uint32_t instruction = mem_read_32(CURRENT_STATE.PC);
 
     switch(get_R_opcode(instruction)){
@@ -82,20 +111,26 @@ void process_instruction()
         case (0b10101010000) : printf("INST ORR (shifted register, shift '00')\n\n"); break;
         case (0b11010010100) : printf("INST MOVZ (hw '00')\n\n"); break;
         case (0b10001011001) : printf("INST ADD (extended register)\n\n"); break;
-        case (0b10110001000) : printf(":D\n\n"); break;
+        // case (0b10110001000) : printf(":D\n\n"); break;
     }
     // printf("INSTRUCTION: %x\n", instruction);
     // printf("OPCODE: %x\n", get_I_opcode(instruction));
     switch(get_I_opcode(instruction)){
-        case (0b1011000100) : 
+        case (0b1011000100): 
             printf("INST ADDS (immediate, shift '00')\n\n");
-            // adds(instruction);
             adds_immediate(instruction, 00);
             break;
         case (0b1011000101) : printf("INST ADDS (immediate, shift '01')\n\n"); break;
-        case (0b1111000100) : printf("INST SUBS (immediate, shift '00')\n\n"); break;
-        case (0b1111000101) : printf("INST SUBS (immediate, shift '01')\n\n"); break;
+        case (0b1111000100):
+            printf("INST SUBS (immediate, shift '00')\n\n");
+            subs_immediate(instruction, 00);
+            break;
+        case (0b1111000101): 
+            printf("INST SUBS (immediate, shift '01')\n\n");
+            subs_immediate(instruction, 01);
+            break;
         case (0b1001000100) : printf("INST ADD (immediate, shift '00')\n\n"); break;
         case (0b1001000101) : printf("INST ADD (immediate, shift '01')\n\n"); break;
     }
+    NEXT_STATE.PC += 4;
 }
