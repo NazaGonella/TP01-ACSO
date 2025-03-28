41a0d579f36c3167e5486006de61bd1cffcc6879
Naza Gonella <nazagonella2@gmail.com>
2025-03-27 22:12:53 -0300

    ehh

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -188,110 +188,6 @@ void blt(uint32_t instruction) {
     }
 }
 
-void ble(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (!(CURRENT_STATE.FLAG_Z == 1 || CURRENT_STATE.FLAG_N == 0)) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
-void bne(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (CURRENT_STATE.FLAG_Z == 0) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
-void bge(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (CURRENT_STATE.FLAG_N == 0) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
-void bgt(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (CURRENT_STATE.FLAG_Z == 0 && CURRENT_STATE.FLAG_N == 0) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
-void ble(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (!(CURRENT_STATE.FLAG_Z == 1 || CURRENT_STATE.FLAG_N == 0)) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
-void bne(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (CURRENT_STATE.FLAG_Z == 0) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
-void bge(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (CURRENT_STATE.FLAG_N == 0) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
-void bgt(uint32_t instruction) {
-    uint32_t imm19 = get_instruction_bit_field(instruction, 19, 5);
-    int64_t offset = (int64_t)(imm19 << 2);
-    if (imm19 & (1 << 18)) {
-        offset |= 0xFFFFFFFFFFE00000;
-    }
-    if (CURRENT_STATE.FLAG_Z == 0 && CURRENT_STATE.FLAG_N == 0) {
-        NEXT_STATE.PC += offset;
-    } else{
-        NEXT_STATE.PC += 4;
-    }
-}
-
 void stur(uint32_t instruction) {
     uint32_t imm9 = get_instruction_bit_field(instruction, 9, 12);
     uint32_t Rn = get_Rn(instruction);
@@ -377,28 +273,6 @@ void add_extended(uint32_t instruction){
 
 }
 
-void bcond(uint32_t instruction){
-    if (get_cond(instruction) == 0b0000) {
-        printf("INST BEQ\n\n");
-        beq(instruction);
-    } else if (get_cond(instruction) == 0b1011) {
-        printf("INST BLT\n\n");
-        blt(instruction);
-    } else if (get_cond(instruction) == 0b0001){
-        printf("INST BNE");
-        bne(instruction);
-    } else if (get_cond(instruction) == 0b1100) {
-        printf("INST BGT");
-        bgt(instruction);
-    } else if (get_cond(instruction) == 0b1010) {
-        printf("INST BGE");
-        bge(instruction);
-    } else if (get_cond(instruction) == 0b1101) {
-        printf("INST BLE");
-        ble(instruction);
-    }
-}
-
 
 Instruction instructions[] = {
     {"INST ADDS (extended register)", 0b10101011000, adds_extended},
@@ -474,8 +348,12 @@ void process_instruction(){
 
     switch (get_instruction_bit_field(instruction, OPCODE_INTERVAL_8)){
         case (0b01010100) :
-            printf("INST BCOND\n\n");
-            bcond(instruction);
-            break;
+            if (get_cond(instruction) == 0b0000) {
+                printf("INST BEQ\n\n");
+                beq(instruction);
+            } else if (get_cond(instruction) == 0b1011) {
+                printf("INST BLT\n\n");
+                blt(instruction);
+            } break;
     }
 }
